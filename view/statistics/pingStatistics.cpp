5d3f0638c4fac9661cf07e5c442ae37f38ea8e2d
dungemeister <yuriy.vazha@gmail.com>
2025-03-06 16:56:57 +0400

    Fixed lost packets %

--- view/statistics/pingStatistics.cpp
+++ view/statistics/pingStatistics.cpp
@@ -5,7 +5,7 @@ void PingStatistics::showStatistics(std::string hostname) {
     std::cout << "--- " << hostname << " --- ping statistics" << "\n";
     std::cout << m_sendedPackets << " packets transmitted, "
               << m_receivedPackets << " packets received, "
-              << static_cast<int>( (m_receivedPackets > 0) ?(1 - m_sendedPackets  / m_receivedPackets) * 100: 100 ) << "\% lost, "
+              << static_cast<int>( (m_receivedPackets > 0) ?(1 - (static_cast<float>(m_receivedPackets) / m_sendedPackets)) * 100: 100 ) << "\% lost, "
               << "time " << m_execTime << "s\n";
     std::cout << std::setprecision(3) << "min_rtt " << m_min_rtt << " avg_rtt " <<
                  m_avg_rtt << " max_rtt " << m_max_rtt << "\n";
